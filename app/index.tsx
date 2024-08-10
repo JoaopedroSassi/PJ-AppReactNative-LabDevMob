14d5f6dc402673493b43c945139bca5e6e7463b7
João Pedro Sassi Granado <54276982+JoaopedroSassi@users.noreply.github.com>
2024-08-10 01:09:17 +0000

    Add projeto

---
+++ app/index.tsx
@@ -0,0 +1,23 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={estilo.container}
+    >
+      <Text style={estilo.text} >Hello World!</Text>
+    </View>
+  );
+}
+
+const estilo = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "black"
+  },
+  text: {
+    color: "red"
+  }
+});
